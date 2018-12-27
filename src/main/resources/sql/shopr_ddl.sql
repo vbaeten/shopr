2601cb83ba7059d1cf9d22c441f6b78d94ddddce
KPRBL94 <kenny.peere@realdolmen.com>
2018-12-27 11:46:41 +0100

    dta.sql aangemaakt en automatisch laten uitvoeren

--- src/main/resources/sql/shopr_ddl.sql
+++ src/main/resources/sql/shopr_ddl.sql
@@ -1,20 +1,5 @@
--- CREATE TABLE user (
---   id                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   name              VARCHAR(255) NOT NULL,
---   first_name        VARCHAR(255) NOT NULL
--- );
-insert into lp(id, title, price, supplierId, artist, genre) values(1, '4 Seasons', 9.99, '1', 'Vivaldi', 'classical');
-insert into lp(id, title, price, supplierId, artist, genre) values(2, 'Fear Of The Dark', 14.99, '3', 'Iron Maiden', 'rock');
-insert into lp(id, title, price, supplierId, artist, genre) values(3, 'Invaders Must Die', 16.99, '2', 'Prodigy', 'dance');
-insert into lp(id, title, price, supplierId, artist, genre) values(4, 'Straight Outta Compton', 12.99, '6', 'N.W.A.', 'hiphop');
-insert into lp(id, title, price, supplierId, artist, genre) values(5, 'Sign O The Times', 14.99, '4', 'Prince', 'rnb');
-insert into lp(id, title, price, supplierId, artist, genre) values(6, 'Commotion', 15.99, '3', 'Creedence Clearwater Revival', 'rock');
-insert into lp(id, title, price, supplierId, artist, genre) values(7, 'Cowboys From Hell', 13.99, '3', 'Pantera', 'rock');
-insert into lp(id, title, price, supplierId, artist, genre) values(8, 'Sheik Yerbouti', 14.49, '3', 'Frank Zappa', 'rock');
-insert into lp(id, title, price, supplierId, artist, genre) values(9, 'Bad', 14.99, '5', 'Michael Jackson', 'pop');
-insert into lp(id, title, price, supplierId, artist, genre) values(10, 'Reverance', 12.50, '2', 'Faithless', 'dance');
-insert into lp(id, title, price, supplierId, artist, genre) values(11, 'Arrival', 6.99, '5', 'Abba', 'pop');
-insert into lp(id, title, price, supplierId, artist, genre) values(12, 'Run DMC', 5.99, '6', 'Run DMC', 'hiphop');
-insert into lp(id, title, price, supplierId, artist, genre) values(13, 'Paul’s Boutique', 10.00, '6', 'Beastie Boys', 'hiphop');
-insert into lp(id, title, price, supplierId, artist, genre) values(14, 'It Takes A Nation Of Millions To Hold Us Back', 5.99, '6', 'Public Enemy', 'hiphop');
-insert into lp(id, title, price, supplierId, artist, genre) values(15, 'Rumours', 14.99, '3', 'Fleetwood Mac', 'rock');
+CREATE TABLE user (
+  id                INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name              VARCHAR(255) NOT NULL,
+  first_name        VARCHAR(255) NOT NULL
+);
