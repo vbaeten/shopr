d950e2dd0e35a95f5d1b891e6b440c375a36fde6
karelrd <karel.debruyne@realdolmen.com>
2018-12-27 09:47:50 +0100

    Delete demo UserRestController and configuration

--- src/main/java/com/realdolmen/shopr/controller/Configuration.java
+++
@@ -1,8 +0,0 @@
-package com.realdolmen.shopr.controller;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class Configuration extends Application {
-}
