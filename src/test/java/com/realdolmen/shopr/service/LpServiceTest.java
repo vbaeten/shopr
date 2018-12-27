3cd6646c59af6bf69cc084eae678bebf09bca305
KPRBL94 <kenny.peere@realdolmen.com>
2018-12-27 15:40:30 +0100

    template werkt niet meer

--- src/test/java/com/realdolmen/shopr/service/LpServiceTest.java
+++ src/test/java/com/realdolmen/shopr/service/LpServiceTest.java
@@ -1,7 +1,41 @@
 package com.realdolmen.shopr.service;
 
+import com.realdolmen.shopr.domain.Lp;
+import com.realdolmen.shopr.repository.LpRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith()
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LpServiceTest {
+    @InjectMocks
+    LpService lpService;
+
+    @Mock
+    LpRepository lpRepository;
+
+    private Lp lp;
+
+    @Before
+    public void init(){
+        lp = new Lp();
+        lp.setId(1);
+        lp.setArtist("Testartist");
+        lp.setTitle("TestTitle");
+        lp.setPrice(9.99);
+    }
+
+    @Test
+    public void testUpdateTitle(){
+        when(lpRepository.findById(1)).thenReturn(lp);
+        lpService.updateTitle(1, "TestTitle");
+        Assert.assertEquals("TestTitle", lp.getTitle());
+    }
+
 }
