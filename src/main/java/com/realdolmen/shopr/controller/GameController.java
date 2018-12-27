50fc97fc13d109f488a1eaa63bb38543c7921416
Inés Van Gucht <39521962+Inesvangucht@users.noreply.github.com>
2018-12-27 16:46:56 +0100

    working on queries

---
+++ src/main/java/com/realdolmen/shopr/controller/GameController.java
@@ -0,0 +1,4 @@
+package com.realdolmen.shopr.controller;
+
+public class GameController {
+}
---
+++ src/main/java/com/realdolmen/shopr/repository/GameRepository.java
@@ -0,0 +1,30 @@
+package com.realdolmen.shopr.repository;
+
+import com.realdolmen.shopr.domain.Game;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class GameReposistory {
+
+    @PersistenceContext(unitName = "shoprPU")
+    private EntityManager entityManager;
+
+    public Game findById(int id) {
+        return entityManager.find(Game.class, id);
+    }
+
+    public List<Game> findAll() {
+        return entityManager.createNamedQuery(Game.FIND_ALL, Game.class).getResultList();
+    }
+
+    public Game findByTitle(String title) {
+        return entityManager.createNamedQuery(Game.FIND_BY_TITLE, Game.class).setParameter("title", title).getSingleResult();
+    }
+
+    public void insert(Game game) {
+        entityManager.persist(game);
+    }
+
+}
