1318024ed7821493eaa18f3dbe2c01f27e1b8b6b
MEETCH <mitch.demeulenaere@realdolmen.com>
2018-12-28 17:09:28 +0100

    styling the app
    MDM

---
+++ src/main/java/com/realdolmen/shopr/beans/ItemBean.java
@@ -0,0 +1,19 @@
+package com.realdolmen.shopr.beans;
+
+
+import com.realdolmen.shopr.controller.GameController;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+@ManagedBean
+@SessionScoped
+public class ItemBean {
+
+
+
+
+
+
+}
---
+++ src/main/webapp/resources/shopr.js
@@ -0,0 +1,10 @@
+function redirectToDetailPage() {
+
+    window.location.href="items-detail.xhtml"
+
+}
+
+function testMe() {
+window.alert("lololol")
+    window.confirm("morelolz")
+}
