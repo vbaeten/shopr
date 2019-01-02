e37dde72604c8fca17e566b5fae63cba1ac02bb1
MeyersK <koenmeyers81@gmail.com>
2019-01-02 08:40:43 +0100

    header bootsfaces now

--- src/main/java/com/realdolmen/shopr/controller/LoginController.java
+++ src/main/java/com/realdolmen/shopr/controller/LoginController.java
@@ -16,19 +16,32 @@ public class LoginController
     EntityManager entityManager;
 
     private User currentUser;
+    public String userName;
 
-    public void setCurrentUser(User user)
+    public void setCurrentUser(int id)
     {
-        this.currentUser = user;
+        this.currentUser = entityManager.find(User.class, id);
+        this.userName = currentUser.getName();
+
     }
 
-    public List<User> findAllUsers()
-    {
-        return entityManager.createQuery("select u from User u", User.class).getResultList();
-    }
+//    public List<User> findAllUsers()
+//    {
+//        return entityManager.createQuery("select u from User u", User.class).getResultList();
+//    }
 
     public User getCurrentUser()
     {
         return currentUser;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
     }
 }
