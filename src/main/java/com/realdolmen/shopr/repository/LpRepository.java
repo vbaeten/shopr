3e2c734ebea9ce6cb377d3d8958301efbc90d082
karelrd <karel.debruyne@realdolmen.com>
2019-01-02 16:48:30 +0100

    Add CRUD methods

--- src/main/java/com/realdolmen/shopr/repository/LpRepository.java
+++ src/main/java/com/realdolmen/shopr/repository/LpRepository.java
@@ -10,7 +10,23 @@ public class LpRepository {
     @PersistenceContext(unitName = "shoprPU")
     private EntityManager entityManager;
 
+    public void insert(Lp lp) {
+        entityManager.persist(lp);
+    }
+
+    public Lp findById(Long id) {
+        return entityManager.find(Lp.class, id);
+    }
+
     public List<Lp> findAll() {
         return entityManager.createNamedQuery(Lp.FIND_ALL, Lp.class).getResultList();
+    }
+
+    public void update(Lp lp) {
+        entityManager.merge(lp);
+    }
+
+    public void delete(Lp lp) {
+        entityManager.remove(lp);
     }
 }
--- src/main/java/com/realdolmen/shopr/service/LpService.java
+++ src/main/java/com/realdolmen/shopr/service/LpService.java
@@ -12,7 +12,24 @@ public class LpService {
     @Inject
     private LpRepository lpRepository;
 
+    public void insert(Lp lp) {
+        lpRepository.insert(lp);
+    }
+
+    public Lp findLpById(Long id) {
+        return lpRepository.findById(id);
+    }
+
     public List<Lp> findAllLps() {
         return lpRepository.findAll();
+    }
+
+    public void update(Lp lp) {
+        lpRepository.update(lp);
+    }
+
+    public void delete(Long id) {
+        Lp lp = lpRepository.findById(id);
+        lpRepository.delete(lp);
     }
 }
